f9fe19b4dbd4f4fa69bc8bf13129dff2bfbe87ef
dcowett <david@davlinalli.com>
2020-12-05 19:55:37 -0500

    Controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.last # hardcode a user for now
+    @article.user = current_user
     @article.save
     if @article.save
       flash[:notice] = "Article was created successfully."
